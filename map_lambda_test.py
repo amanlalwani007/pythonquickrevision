73be4a6a6600bc1e23a1cc3cba9a9e888e200a42
amanlalwani007 <amanlalwani0807@gmail.com>
2024-05-02 15:00:17 +0530

    added

---
+++ map_lambda_test.py
@@ -0,0 +1,11 @@
+a=[1,2,3 ,4]
+print (list(map(lambda  x :x*2, a)))
+
+
+add= lambda x,y:x+y
+result= add(5,3)
+print(result)
+
+# lambda function are anonymous function , consisw way to 
+# create function without using def keyword
+
